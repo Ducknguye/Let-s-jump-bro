bd7b5a767886529b565719f20f60c13050de0472
Duc Nguyen <duc.nguyen@software.codegym.vn>
2026-03-26 19:07:05 +0700

    thêm anim win và main menu

--- Assets/Script/GoalController.cs
+++ Assets/Script/GoalController.cs
@@ -2,29 +2,20 @@ using UnityEngine;
 
 public class GoalController : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem left;
+    [SerializeField] private ParticleSystem right;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            // 🔊 phát sound win
+            // 🔊 sound
             SoundManager.instance.musicSource.Stop();
             SoundManager.instance.PlaySFX(SoundManager.instance.win);
 
-            // 🛑 dừng hoàn toàn player
-            PlayerController player = collision.GetComponent<PlayerController>();
-            if (player != null)
-            {
-                player.enabled = false;
+            left.Play();
+            right.Play();
 
-                Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
-                if (rb != null)
-                {
-                    rb.linearVelocity = Vector2.zero;   // dừng di chuyển
-                    rb.bodyType = RigidbodyType2D.Kinematic; // tắt vật lý
-                }
-            }
-
-            // 🎉 debug
             Debug.Log("YOU WIN!");
         }
     }
